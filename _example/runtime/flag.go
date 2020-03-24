5729011e624c0c9a2a2223778a45953136ff7a2c
Sam Xie <xsambundy@gmail.com>
2020-03-24 16:36:31 +0800

    docs: update example

---
+++ _example/runtime/flag.go
@@ -0,0 +1,11 @@
+package runtime
+
+type Flag struct {
+	Number      int `flag:"env"`
+	Environment Environment
+}
+
+type Environment struct {
+	DevelopmentMode bool `flag:"env short=d" flag-usage:"change environment mode to development"`
+	JSONLogStyle    bool `flag:"env name=json-log-style short=j" flag-usage:"change log style to JSON"`
+}
